436f77a9a1812ecc58905b13c34f550ede24f627
vgentile98 <147622625+vgentile98@users.noreply.github.com>
2024-05-19 00:44:29 +0200

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -6,7 +6,6 @@ import transformers
 from transformers import CamembertTokenizer, CamembertForSequenceClassification, pipeline
 import sentencepiece
 import tokenizers
-import protobuf
 
 st.title('Levelingo')
 st.write('Welcome to Levelingo!')
