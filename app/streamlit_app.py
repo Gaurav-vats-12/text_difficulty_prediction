694f90a7f78bfc2968fc4f87bdc79331a3f30d62
vgentile98 <147622625+vgentile98@users.noreply.github.com>
2024-05-18 17:12:51 +0200

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -5,8 +5,20 @@ import requests
 st.title('Levelingo')
 st.write('Welcome to Levelingo!')
 
+# Load the model
+def load_model():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = CamembertForSequenceClassification.from_pretrained('path_to_model')
+    model.to(device)
+    model.eval()
+    return model, device
+
+model, device = load_model()
+tokenizer = CamembertTokenizer.from_pretrained('path_to_tokenizer')
+
+# Fetch french texts via newsapi
 def fetch_french_news(api_key):
-    url = f"https://newsapi.org/v2/top-headlines?country=fr&apiKey={api_key}"
+    url = f"https://newsapi.org/v2/top-headlines?country=fr&apiKey=e7c7cca4d5184b069f195de63ad0d86c"
     response = requests.get(url)
     articles = response.json().get('articles', [])
     french_texts = [article['content'] for article in articles if article['content']]
