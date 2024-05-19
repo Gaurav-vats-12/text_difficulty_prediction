2f0c67d499cb33f5278e46f7de751237802455af
vgentile98 <147622625+vgentile98@users.noreply.github.com>
2024-05-19 02:30:25 +0200

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -69,8 +69,8 @@ def main():
                     st.subheader(article['title'])
                     st.write(article['description'] if article['description'] else 'No description available.')
                     # Button to open article in an iframe within the app
-                    if st.button("Read Now", key=article['title']):
-                        components.iframe(article['url'], height=600)
+                    with st.expander("Read Now"):
+                        components.iframe(article['url'], height=300)
                     st.markdown("---")
     else:
         st.write("No articles found. Try adjusting your filters.")
