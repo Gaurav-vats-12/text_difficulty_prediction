6c79f2493f8dd9bacf6d452fa0eb53ab34c91da1
vgentile98 <147622625+vgentile98@users.noreply.github.com>
2024-05-21 17:30:34 +0200

    Update app_copy.py

--- app/app_copy.py
+++ app/app_copy.py
@@ -166,9 +166,8 @@ def main():
 
         articles = fetch_news(category)
         if articles:
-            model, tokenizer = setup_model()
             if model and tokenizer:
-                articles = assign_article_levels(articles, model, tokenizer)
+                articles = assign_article_levels(articles)
                 articles = [article for article in articles if article['level'] == user_level and is_valid_image_url(article['image'])]
                 for idx, article in enumerate(articles):
                     with st.container():
